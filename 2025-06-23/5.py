6a4bed17159c326c8cf9ba5e219b3fe4361da60b
andsta2025 <andrius.staras@gmail.com>
2025-06-23 19:14:37 +0100

    pridėjau python failu

---
+++ 2025-06-23/5.py
@@ -0,0 +1,4 @@
+colors = ("red", "green", "blue", "yellow", "purple", "orange")
+print ("element of the tuple:", colors[2])  # Print 3element
+index_of_color = colors.index("orange")  # Find the index of "orange"
+print ("Index of 'orange':", index_of_color)  # Print the index of "orange"
