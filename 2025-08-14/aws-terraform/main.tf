434987985e08ef5f195190086622ef994c3e4d16
andsta2025 <andrius.staras@gmail.com>
2025-08-14 20:49:38 +0300

    2025-08-14

---
+++ 2025-08-14/aws-terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.38"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-042b4708b1d05f512"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
